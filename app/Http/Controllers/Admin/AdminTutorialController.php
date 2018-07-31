323dbca7ecd5aa3ff1e6a25b827fb087a25e6e71
Puspitavabna <41649263+Puspitavabna@users.noreply.github.com>
2018-07-31 21:29:11 +0600

    Reformat the code of admin tutorial index

--- app/Http/Controllers/Admin/AdminTutorialController.php
+++ app/Http/Controllers/Admin/AdminTutorialController.php
@@ -4,11 +4,12 @@
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 
+use App\Models\Tutorial;
 use Illuminate\Http\Request;
 use Illuminate\Support\facades\Redirect;
 use DB;
 use Session;
-
+use Auth;
 class AdminTutorialController extends Controller
 {
     public function index(){
@@ -18,15 +19,13 @@ class AdminTutorialController extends Controller
         return view ('admin.tutorials.create');
     }
     public function store(Request $request){
-      $data=array();
-          	$data['title']=$request->title;
-          	$data['description']=$request->description;
-            $data['category_id']=$request->category_id;
-            $id = Auth::user()->id;
-            $currentuser = User::find($id);
-             $data['user_id']=$request->user_id;
-          	DB::table('tutorials')->insert($data);
-                	Session::put('exception','Tutorials added successfully!!');
-                	return Redirect::to('/admin.tutorials.index');
+        $tutorial = new Tutorial();
+        $tutorial->title = $request->title;
+        $tutorial->description = $request->description;
+        $tutorial->category_id = $request->category_id;
+        $tutorial->user_id = Auth::user()->id;
+        $tutorial->save();
+        Session::flash('success','Tutorials added successfully!!');
+        return redirect()->route('admin_tutorial.index');
     }
 }
